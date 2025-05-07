e4a8584a5961bb05219bc17ff8f7b3a56bd2fb2b
svivek19 <svivek030503@gmail.com>
2025-05-07 17:45:25 +0530

    feat: implement getTasks endpoint and update task routes

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -12,3 +12,17 @@ export const createTask = async (req, res) => {
       .json({ message: "Failed to create task", error: error.message });
   }
 };
+
+export const getTasks = async (req, res) => {
+  try {
+    const tasks = await Tasks.find();
+    if (!tasks || tasks.length === 0) {
+      return res.status(404).json({ message: "No tasks found" });
+    }
+    return res.status(200).json(tasks);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to fetch tasks", error: error.message });
+  }
+};
--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createTask } from "../controllers/task.controller.js";
+import { createTask, getTasks } from "../controllers/task.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const taskRouter = express.Router();
 
+taskRouter.get("/get-all", authMiddleware, getTasks);
 taskRouter.post("/create", authMiddleware, createTask);
 
 export default taskRouter;
